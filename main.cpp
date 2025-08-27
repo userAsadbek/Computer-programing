d6fdf17c04f86b7ded223307e45c1ad3e9a2f740
250123 <250123@newuu.uz>
2025-08-27 11:44:30 +0500

    just codes

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+ using namespace std;// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+
+    cout<<"My name is Heisnburg ";
+    cout<<"and Im hambuerger ";
+    return 0;
+}
